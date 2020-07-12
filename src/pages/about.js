ce43ed0a47b21aa764e38179e4a62dda38a75ab3
Pavlos Khetab <pavlos.khetab@gmail.com>
2020-07-12 10:31:53 +0100

    Added a css module with a container component. I also modified the page component about.js to include a list of people biographies and created a css file for that called about.module.css.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./about.module.css"
 import Header from "../components/header"
+import Container from "../components/container"
+
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <div style={{ color: `black` }}>
       <Link to="/">Home</Link> 
+      <Container>
       <Header headerText="About Eyegor" />
       <Header headerText="About The Doc" />     
       <p>Such magnificence. Very awesome.</p>
+
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      </Container>
     </div>
   )
 }
