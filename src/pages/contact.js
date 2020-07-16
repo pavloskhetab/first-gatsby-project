ec7762676f170698144afef10b786e6af98ccf52
Pavlos Khetab <pavlos.khetab@gmail.com>
2020-07-16 22:52:56 +0100

    Removed old Link uses in about, contact and index.js files and created a shared ListLink in layout component.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
 export default function Contact() {
   return (
     <Layout style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
       <Header headerText="Contact" />
       <p>Give us a shout!</p>
       <a href="mailto:me@example.com">eyeygor@yoyo.com</a>
