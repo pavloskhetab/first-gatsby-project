6a04fe3049c8e8f7379492d8237866a70af87ada
Pavlos Khetab <pavlos.khetab@gmail.com>
2020-07-17 12:00:48 +0100

    Separated welcome text into two headerText.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
     <Layout style={{ color: "purple" }}>
-      <Header headerText="Hello my friends! Welcome to Pavlos' website." />
+      <Header headerText="Hello my friends!" />
+      <Header headerText="Welcome to Pavlos' website." />
       <p>Very exciting!</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <img
+        src="https://source.unsplash.com/random/400x200"
+        alt="Random image appeared."
+      />
     </Layout>
   )
 }
